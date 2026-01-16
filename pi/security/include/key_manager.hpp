962e20243cde39f3c2e76cf2d6053a7d24de99a5
bryce <bryceclark237@gmail.com>
2026-01-16 15:28:07 -0800

    migrate key manager to libsodium

--- pi/security/include/key_manager.hpp
+++ pi/security/include/key_manager.hpp
@@ -1,25 +1,19 @@
-#include <openssl/rand.h>
-#include <openssl/err.h>
-#include <openssl/evp.h>
+#include <sodium.h>
 #include <array>
 
 #pragma once
 
+// TODO - the TAK should be manually shared between the server and client device, but we need to figure out a protocol for this (ex: USB, manually transfered config file, etc.)
+
 class KeyManager
 {
     public:
-		const std::string FEK_SALT = "filebox_fek";
-		const std::string FEK_INFO = "file_encryption_v1";
-
-		const std::string TAK_SALT = "filebox_tak";
-		const std::string TAK_INFO = "transport_auth_v1";
+		const std::string FEK_CONTEXT = "file_encryption_v1";
+		const std::string TAK_CONTEXT = "transport_auth_v1";
 
 		// TODO - the key is only generated once on first boot, after that we need to load it
-        std::array<uint8_t, MDK_SIZE_BYTES> gen_mdk();
+        uint8_t* gen_mdk();
 
-		bool derive_key(const uint8_t* mdk, size_t mdk_len, uint8_t* key_out, size_t key_len, std::string salt, std::string info);
-
-    private:
-        const size_t MDK_SIZE_BYTES = 32;
+		void derive_key(const uint8_t* mdk, uint8_t* key_out, size_t key_len, std::string context);
 };
 
--- pi/security/src/key_manager.cpp
+++ pi/security/src/key_manager.cpp
@@ -1,37 +1,14 @@
 #include "key_manager.hpp"
 
-std::array<uint8_t, KeyManager::MDK_SIZE_BYTES> KeyManager::gen_mdk()
+uint8_t* KeyManager::gen_mdk()
 {
-    std::array<uint8_t, MDK_SIZE_BYTES> key{};
+    uint8_t key_buf[crypto_kdf_KEYBYTES];
+    crypto_kdf_keygen(key_buf);
 
-    if (RAND_bytes(key, MDK_SIZE_BYTES) != 1) {
-        std::cerr << "Failed to generate random bytes" << std::endl;
-        ERR_print_errors_fp(stderr);
-    }
-
-    return key;
+    return key_buf;
 }
 
-bool KeyManager::derive_key (const uint8_t* mdk, size_t mdk_len, uint8_t* key_out, size_t key_len, std::string salt, std::string info)
+void KeyManager::derive_key(const uint8_t* mdk, uint8_t* key_out, size_t key_len, std::string context, uint64_t subkey_id)
 {
-	EVP_PKEY_CTX* pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_HKDF, nullptr);
-	if (!pctx) return false;
-
-	if (EVP_PKEY_derive_init(pctx) <= 0) return false;
-	if (EVP_PKEY_CTX_set_hkdf_md(pctx, EVP_sha256()) <= 0) return false;
-
-	if (EVP_PKEY_CTX_set1_hkdf_salt(pctx, 
-		reinterpret_cast<const uint8_t*>(salt), 
-		strlen("filebox_fek")) <= 0) return false;
-
-	if (EVP_PKEY_CTX_set1_hkdf_key(pctx, mdk, mkd_len) <= 0) return false;
-	if (EVP_PKEY_CTX_add1_hkdf_info(pctx, 
-		reinterpret_cast<const uint8_t*>(info), 
-		strlen("file_encryption_v1")) <= 0) return false;
-
-	size_t len = key_len;
-	if (EVP_PKEY_derive(pctx, key_out, &len) <= 0) return false;
-
-	EVP_PKEY_CTX_free(pctx);
-	return true;
+	crypto_kdf_derive_from_key(key_out, key_len, subkey_id, context, mdk);
 }
